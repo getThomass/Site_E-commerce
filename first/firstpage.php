8395478301a436c08b3e80de4dfe7fd1b96a0340
paulo13320 <63582235+paulo13320@users.noreply.github.com>
2020-04-17 17:20:06 +0200

    Add files via upload

---
+++ first/firstpage.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+	 <title> Ebay ECE</title>
+		 <link rel="stylesheet" a href="firstpage.css">
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+		<body>
+		 	<div class="container">
+		 		<img src="bonhomme.jpg" alt="profil">
+		 		
+		 			<form method="POST" action="#">
+		 	<div class="form-input">
+		 		<i class="fa fa-user fa-2x cust" aria-hidden="true"></i>
+		 		
+		 		<input type="text" name="login" placeholder="Nom d'utilisateur"> <br />
+
+		 		<i class="fa fa-lock fa-2x cust" aria-hidden="true"></i>
+		 	
+		 		<input type="password" name="password" placeholder="Mot de passe"><br />
+		 	
+		 		
+		 		<input type="submit" type="submit" value="Connexion" class="btn-connexion"><br /><br />
+		 		
+		 		<a class="inscrit" href="#"> Pas encore inscrit ?</a>
+		 			
+		 	</div>
+		 			</form>
+		</body>
+	</html>

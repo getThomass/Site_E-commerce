003f8b9cdc560c50abc9eacf89ebe4cc7f7d3bb2
Thomas COSTA <61493790+Thomas-code-ECE@users.noreply.github.com>
2020-04-17 11:39:01 +0200

    Add files via upload

---
+++ page_connexion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Connexion</title>
+</head>
+<body>
+	<form method="POST" action="page_verification.php">
+		<input type="text" name="email" placeholder="Adresse mail"/></br>
+		<input type="password" name="mdp" placeholder="Mot de passe"/></br>
+		<input type="submit" name="valider" value="connexion"/>
+	</form>
+	<div id="inscription">Nouveau par ici ? <a href="http://localhost/Projet_piscine/Inscription/Cr%c3%a9ation_compte.html">Inscrivez-vous</a></div>
+</body>
+</html>

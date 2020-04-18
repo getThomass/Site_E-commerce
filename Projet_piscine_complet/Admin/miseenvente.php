fcb74c4cfaf7efb736dc61367e95b097480e83be
Thomas COSTA <61493790+Thomas-code-ECE@users.noreply.github.com>
2020-04-18 11:01:20 +0200

    Update miseenvente.php

--- Projet_piscine_complet/Admin/miseenvente.php
+++ Projet_piscine_complet/Admin/miseenvente.php
@@ -24,18 +24,21 @@
 					if($("#meilleur_offre").is($(":checked"))) {
 						$("#meilleur_offre").prop("checked",false);
 					}
+                    $("#date_fin_enchere").css("visibility","visible");
 				}
 				$("#validation").prop("disabled",false);
 				if($(this).is($(":checked"))==false){
 					if($("#achat_immediat").is($(":checked"))==false && $("#meilleur_offre").is($(":checked"))==false){
 						$("#validation").prop("disabled",true);
 					}
+                    $("#date_fin_enchere").css("visibility","hidden");
 				}
 			}));
 			if($("#meilleur_offre").change(function(){
 				if($(this).is($(":checked"))){
 					if($("#encherir").is($(":checked"))) {
 						$("#encherir").prop("checked",false);
+                        $("#date_fin_enchere").css("visibility","hidden");
 					}
 				}
 				$("#validation").prop("disabled",false);
@@ -80,6 +83,7 @@
                             Achat immédiat<input type="checkbox" name="type_1" id="achat_immediat" style="margin-left:20px"/></br>
 	                        Enchérir<input type="checkbox" name="type_2" id="encherir" style="margin-left:75px"/></br>
 	                        Meilleur offre<input type="checkbox" name="type_3" id="meilleur_offre" style="margin-left:36px"/></br> 
+                            <input type="date" name="date_fin_enchere" id="date_fin_enchere" style="visibility:hidden"/>
                         </td>
                     </tr>
                     <tr>

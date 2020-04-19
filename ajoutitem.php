e1d4823c83506ca2cac9f3d18e0e5cb35283e1ff
val-avatar <63727086+val-avatar@users.noreply.github.com>
2020-04-19 10:55:13 +0200

    Add files via upload

--- ajoutitem.php
+++ ajoutitem.php
@@ -13,6 +13,9 @@ $nom = isset($_POST["nom"])? $_POST["nom"] : "";
 $description = isset($_POST["description"])? $_POST["description"] : "";
 $categorie = isset($_POST["categorie"])? $_POST["categorie"] : "";
 $prix = isset($_POST["prix"])? $_POST["prix"] : "";
+$date_fin = isset($_POST["date_fin_enchere"])? $_POST["date_fin_enchere"] : "";
+$date_debut = date('Y-m-d');
+
 
 if(isset($_POST['type_1'])){
     $bdd->exec('INSERT INTO article(Type_vente1) VALUES(\'1\')');
@@ -33,19 +36,26 @@ while($donnees=$result->fetch()){
 }
 $result->closeCursor();
 
-$file = $_FILES["image"];
-move_uploaded_file($file["tmp_name"], "Photo_article" . $file["name"]);
-$nom_image = $_FILES["image"]["name"];
+$chemin=getcwd();
+
+$name_photo=$_FILES["image"]["name"];
+$tmp_photo=$_FILES["image"]["tmp_name"];
+move_uploaded_file($tmp_photo,"Photo_article/".$name_photo);	
+copy($chemin."\Photo_article\\".$name_photo,str_replace("Admin","Vente\\",$chemin)."Photo_article\\".$name_photo);
+					
+
 
 
  ///permet de rechercher l'erreur lié à la base de donnée
-$req = $bdd->prepare('INSERT INTO article(Nom_article, lien_photo_item, Description, Categorie, Prix, ID_vendeur) VALUES(:Nom_article, :lien_photo_item, :Description, :Categorie, :Prix, :ID_vendeur)');
+$req = $bdd->prepare('INSERT INTO article(Nom_article, lien_photo_item, Description, Categorie, Prix, Date_debut, Date_fin, ID_vendeur) VALUES(:Nom_article, :lien_photo_item, :Description, :Categorie, :Prix, :Date_debut, :Date_fin, :ID_vendeur)');
 $req->execute(array(
 	'Nom_article' => $nom,
-	'lien_photo_item' => $nom_image,
+	'lien_photo_item' => $name_photo,
 	'Description' => $description,
 	'Categorie' => $categorie,
     'Prix' => $prix,
+    'Date_debut' => $date_debut,
+    'Date_fin' => $date_fin,
     'ID_vendeur' => $vendeur
     ));
 
